095e4ded207fa80cf588842e9df0ec773aa5b8ba
Travis Brown <travis.brown@nidavellir.in>
2022-10-05 10:56:07 -0400

    Adding ConfigurationKeyName attribute to configuration properties

--- Configuration/AwsSimpleStorageServiceClientConfiguration.cs
+++ Configuration/AwsSimpleStorageServiceClientConfiguration.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 using System.Xml.Serialization;
+using Microsoft.Extensions.Configuration;
 
 // Define the namespace
 namespace SyncStream.Aws.S3.Client.Configuration;
@@ -13,6 +14,7 @@ public class AwsSimpleStorageServiceClientConfiguration : IAwsSimpleStorageServi
     /// <summary>
     ///     This property contains the AWS access key
     /// </summary>
+    [ConfigurationKeyName("accessKeyId")]
     [JsonPropertyName("accessKeyId")]
     [XmlAttribute("accessKeyId")]
     public string AccessKeyId { get; set; }
@@ -20,6 +22,7 @@ public class AwsSimpleStorageServiceClientConfiguration : IAwsSimpleStorageServi
     /// <summary>
     ///     This property contains the AWS KMS Key ID for encrypting and decrypting objects
     /// </summary>
+    [ConfigurationKeyName("keyManagementServiceKeyId")]
     [JsonPropertyName("keyManagementServiceKeyId")]
     [XmlAttribute("keyManagementServiceKeyId")]
     public string KeyManagementServiceKeyId { get; set; }
@@ -27,6 +30,7 @@ public class AwsSimpleStorageServiceClientConfiguration : IAwsSimpleStorageServi
     /// <summary>
     ///     This property contains the AWS secret access key
     /// </summary>
+    [ConfigurationKeyName("secretAccessKey")]
     [JsonPropertyName("secretAccessKey")]
     [XmlAttribute("secretAccessKey")]
     public string SecretAccessKey { get; set; }
@@ -34,6 +38,7 @@ public class AwsSimpleStorageServiceClientConfiguration : IAwsSimpleStorageServi
     /// <summary>
     ///     This property contains the AWS region
     /// </summary>
+    [ConfigurationKeyName("region")]
     [JsonPropertyName("region")]
     [XmlAttribute("region")]
     public string Region { get; set; }
